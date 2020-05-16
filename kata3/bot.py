e4ea3588ed35d6fee567a9e489c9d67e186d4b89
Sergi Oliveres <solive@uic.es>
2020-05-16 13:48:13 +0200

    Bot fucionando;

--- kata3/bot.py
+++ kata3/bot.py
@@ -1,12 +1,29 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 
 def main():
-    updater = Updater(token = open('./bot_token').read(), use_context = True)
-    
-    # preguntar a telegram si hay nuevas notificaciones
+    # instantiate a new Updater, passing in the token in constructor
+    updater = Updater(token = '1189835632:AAFZHwz60i6WuFMlCbaZIeqInDUqk5zpk20', use_context = True)
+
+    # add a new command handler
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('repeat', repeat))
+    updater.dispatcher.add_handler(CommandHandler('sum', sum))
+
+    # ask telegram for new notifications
     updater.start_polling() 
     
-    # si alguien corta la ejecución del bot, esta función idle permite que acabe
+    # capture stop-lights
     updater.idle()  
+
+def start(update, context):
+    update.message.reply_text('Hola, soy un bot hecho con Python!')
+    
+def repeat(update, context):
+    update.message.reply_text(update.message.text)
+
+def sum(update, context):
+    result = int(context.args[0]) + int(context.args[1])
+    result = str(result)
+    update.message.replay_text("El resultado es " + result)
 
 main()
